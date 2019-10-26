2eaee813a4632f3ad4ecf82078e72b2dca4bf786
mincheerleader <34811121+mincheerleader@users.noreply.github.com>
2019-10-26 23:02:17 +0800

    Add files via upload

---
+++ Project/misc.d
@@ -0,0 +1,13 @@
+.\misc.o: ..\Libraries\src\misc.c
+.\misc.o: ..\Libraries\inc\misc.h
+.\misc.o: ..\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+.\misc.o: ..\CMSIS\CM3\CoreSupport\core_cm3.h
+.\misc.o: D:\MDK\mdk\ARM\ARMCC\Bin\..\include\stdint.h
+.\misc.o: ..\CMSIS\CM3\DeviceSupport\ST\STM32F10x\system_stm32f10x.h
+.\misc.o: ..\code\stm32f10x_conf.h
+.\misc.o: ..\Libraries\inc\stm32f10x_gpio.h
+.\misc.o: ..\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+.\misc.o: ..\Libraries\inc\stm32f10x_rcc.h
+.\misc.o: ..\Libraries\inc\stm32f10x_spi.h
+.\misc.o: ..\Libraries\inc\stm32f10x_usart.h
+.\misc.o: ..\Libraries\inc\misc.h
---
+++ Project/mmc_sd.d
@@ -0,0 +1,15 @@
+.\mmc_sd.o: ..\code\mmc_sd.c
+.\mmc_sd.o: ..\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+.\mmc_sd.o: ..\CMSIS\CM3\CoreSupport\core_cm3.h
+.\mmc_sd.o: D:\MDK\mdk\ARM\ARMCC\Bin\..\include\stdint.h
+.\mmc_sd.o: ..\CMSIS\CM3\DeviceSupport\ST\STM32F10x\system_stm32f10x.h
+.\mmc_sd.o: ..\code\stm32f10x_conf.h
+.\mmc_sd.o: ..\Libraries\inc\stm32f10x_gpio.h
+.\mmc_sd.o: ..\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+.\mmc_sd.o: ..\Libraries\inc\stm32f10x_rcc.h
+.\mmc_sd.o: ..\Libraries\inc\stm32f10x_spi.h
+.\mmc_sd.o: ..\Libraries\inc\stm32f10x_usart.h
+.\mmc_sd.o: ..\Libraries\inc\misc.h
+.\mmc_sd.o: ..\code\mmc_sd.h
+.\mmc_sd.o: ..\code\bsp_usart2.h
+.\mmc_sd.o: D:\MDK\mdk\ARM\ARMCC\Bin\..\include\stdio.h
